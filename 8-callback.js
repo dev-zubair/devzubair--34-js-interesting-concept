abd18ff3d4aa83631810efed38b0ad45d01c16ae
zubairhmd <zubairhmd84@gmail.com>
2021-08-30 18:17:11 +0600

    8

---
+++ 8-callback.js
@@ -0,0 +1,30 @@
+// Example 1
+function welcomeMessage(name, greetHandler) {
+    greetHandler(name);
+}
+// const names = ['Tom Hanks', 'Tom Brady', 'Tom Cruise']
+// const myObj = { name: 'Tom Chinku', age: 11 };
+function greetMorning(name) {
+    console.log('Good morning', name);
+}
+function greetEvening(name) {
+    console.log('Good Evening', name);
+}
+function greetAfternoon(name) {
+    console.log('Good afternoon', name);
+}
+welcomeMessage('Tom Hanks', greetMorning);
+welcomeMessage('Sakib Hanks', greetAfternoon);
+welcomeMessage('Bappa Raj', greetEvening);
+
+
+// Example 2 
+function handleClick() {
+    console.log('button is clicked');
+}
+document.getElementById('my-btn').addEventListener('click', handleClick)
+
+// Example 3
+document.getElementById('btn').addEventListener('click', function () {
+    console.log('buttn is clicked');
+})
