73df79070a5abee0c523ed1d942d3b9ed2d360de
zubairhmd <zubairhmd84@gmail.com>
2021-08-30 15:10:26 +0600

    truthy

--- 3-true-false.js
+++ 3-true-false.js
@@ -4,12 +4,18 @@ FALSY :
 false
 0 (if number is 0)
 empty string 
+undefined
+Null
+NAN
 
 --------------------
 Truthy:
 truth
 any number (positive or Nagative)
 any string including single whitespace (as like = " " or "0")
+[]
+{}
+Anything else that is not falsy will be TRUTHY
 ---------
 */
 
@@ -60,6 +66,57 @@ else {
 
 //Empty String
 const x = " ";
+if (x) {
+    console.log('variable is truthy');
+}
+else {
+    console.log('variable is falsy');
+}
+
+
+//undefined - kono ekta veriable ke jodi declare na kori tahole seta undefined hoy.kono kisu undefined hole FALSE hoy jay.
+let x;
+console.log('value of x', x);
+if (x) {
+    console.log('variable is truthy');
+}
+else {
+    console.log('variable is falsy');
+}
+
+//Null
+let x = null;
+console.log('value of x', x);
+if (x) {
+    console.log('variable is truthy');
+}
+else {
+    console.log('variable is falsy');
+}
+
+//NAN
+let x = parseInt('Hello');
+console.log('value of x', x);
+if (x) {
+    console.log('variable is truthy');
+}
+else {
+    console.log('variable is falsy');
+}
+
+//Empty Array
+let x = [];
+console.log('value of x', x);
+if (x) {
+    console.log('variable is truthy');
+}
+else {
+    console.log('variable is falsy');
+}
+
+//Empty Object
+let x = {};
+console.log('value of x', x);
 if (x) {
     console.log('variable is truthy');
 }
